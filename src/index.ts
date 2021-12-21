2dfbfce4991fe55ec048bb369c886a67df0d5b5c
Andreas Rossa <andreas@rossamail.de>
2021-12-21 19:19:03 +0100

    Implement event handling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import { Server } from "socket.io";
+import express from "express";
+import http from "http";
+import turtleEventHandlers from "./events";
+import _ from "lodash"
 
-const io = new Server(3000, {
+const PORT = 3000
 
-})
+const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server)
 
 io.on("connection", (socket) => {
-	socket.send("Welcome!")
+	console.log(`Client connected: #${socket.id}`)
+	socket.broadcast.emit(`Client connected: #${socket.id}`)
+	Object.entries(turtleEventHandlers).forEach(([e, handler]) => {
+		const getListener = _.curry(handler)(socket)
+		socket.addListener(e, getListener)
+	})
+})
+
+server.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}...`)
 })
