bffa41161d521c50c254c42f6d5c7b6cc5defc06
Andreas Rossa <andreas@rossamail.de>
2021-12-24 02:11:16 +0100

    Add test result display for github actions

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,12 @@
-import type {Config} from '@jest/types';
+import type {Config} from "@jest/types";
 
 // Sync object
 const config: Config.InitialOptions = {
-  verbose: true,
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
+	verbose: true,
+	transform: {
+		"^.+\\.ts?$": "ts-jest",
+	},
+	reporters: ["default", "jest-junit"],
 };
 
 export default config;
